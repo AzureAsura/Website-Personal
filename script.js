282fcac528b432ce9fb13b95177fc5c25e0cc1c3
Azure <paramasuraqutay@gmail.com>
2025-03-19 12:28:04 +0800

    script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Yakin?",
-    "Beneran??",
-    "Fix gamau nih?",
-    "Abel plis...",
-    "Pikirin mateng mateng!",
-    "Kalo kamu bilang engga, galau aku...",
-    "Beneran ni galau aku...",
-    "Galau segalau galaunya...",
-    "Oh, okedeh...",
-    "Becanda , iyain plis! ❤️"
+    "Are you sure?",
+    "Really sure??",
+    "Are you positive?",
+    "Pookie please...",
+    "Just think about it!",
+    "If you say no, I will be really sad...",
+    "I will be very sad...",
+    "I will be very very very sad...",
+    "Ok fine, I will stop asking...",
+    "Just kidding, say yes please! ❤️"
 ];
 
 let messageIndex = 0;
